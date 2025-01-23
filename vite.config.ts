c59c886c6c3ab4ba4a6e6d8cd8f981fafe99acc0
FudouRyouho <fudozaraki001@hotmail.com>
2025-01-22 21:12:19 -0300

    v0.01a

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from '@vercel/remix/vite';
+import { vercelPreset } from "@vercel/remix/vite";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -23,4 +23,12 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  build: {
+    minify: "terser",
+    rollupOptions: {
+      treeshake: {
+        moduleSideEffects: 'no-external', // Optimiza el tree shaking para módulos externos
+      },
+    },
+  },
 });
